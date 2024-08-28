ad53370ca775b32c7e739e670024f17469f45240
“Nahuel <nnahuell.gb@hotmail.com>
2024-08-28 13:07:58 -0300

    angular newbe proyect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,34 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule], // Añade CommonModule a imports
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] 
 })
 export class AppComponent {
-  title = 'nombreDelProyecto';
+  contador = 0;
+  bonus = false;
+  tablaPuntuaciones = [
+    { nombre: 'Juan', puntuacion: 855 },
+    { nombre: 'Pedro', puntuacion: 803 },
+    { nombre: 'Luis', puntuacion: 720 },
+  ];
+
+  sumar(cantidad: number): void {
+    this.contador = this.contador += cantidad;
+
+    // Cuando alcanzamos 10, habilita un botón de bonus
+    if (this.contador === 10) {
+      this.bonus = true;
+
+      // Timeout de 5 segundos, pasado este tiempo el botón volverá a su estado original
+      window.setTimeout(() => {
+        this.bonus = false;
+      }, 5000);
+    }
+  }
 }
